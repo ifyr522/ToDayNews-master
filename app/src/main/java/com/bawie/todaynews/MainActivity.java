ecddd609a02e6de637865440a16ee3c65c947f48
lf <lf>
2017-05-09 19:14:27 +0800

    Initial commit

---
+++ app/src/main/java/com/bawie/todaynews/MainActivity.java
@@ -0,0 +1,52 @@
+package com.bawie.todaynews;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.bawie.todaynews.fragment.MentLeftFragment;
+import com.bawie.todaynews.fragment.MenuRightFragment;
+import com.bwei.slidingmenu.SlidingMenu;
+import com.bwei.slidingmenu.app.SlidingFragmentActivity;
+
+/**
+ * 1.类的作用：主页面
+ */
+public class MainActivity extends SlidingFragmentActivity {
+    private SlidingMenu slidingMenu;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initLeftRight();
+    }
+
+    private void initLeftRight() {
+        Fragment leftFragment=new MentLeftFragment();
+        setBehindContentView(R.layout.left_menu_frame);
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame_leftmenu_id,leftFragment).commit();
+        slidingMenu=getSlidingMenu();
+        //设置滑动方式
+        slidingMenu.setMode(SlidingMenu.LEFT_RIGHT);
+        // 设置触摸屏幕的模式
+        slidingMenu.setTouchModeAbove(SlidingMenu.TOUCHMODE_MARGIN);
+        // 设置阴影的宽度
+        slidingMenu.setShadowWidthRes(R.dimen.shadow_width);
+        // 设置slidingmenu边界的阴影图片
+        slidingMenu.setShadowDrawable(R.drawable.shadow);
+        // 设置滑动菜单视图的宽度
+        slidingMenu.setBehindOffsetRes(R.dimen.slidingmenu_offset);
+        // 设置渐入渐出效果的值
+        slidingMenu.setFadeDegree(0.35f);
+
+        //设置右边（二级）侧滑菜单
+        MenuRightFragment rightMenuFragment = new MenuRightFragment();
+//        rightMenuFragment.setShareListener(this);
+        slidingMenu.setSecondaryMenu(R.layout.right_menu_frame);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.id_frame_rightmenu, rightMenuFragment).commit();
+
+    }
+}
+
+
---
+++ app/src/main/java/com/bawie/todaynews/fragment/MentLeftFragment.java
@@ -0,0 +1,25 @@
+package com.bawie.todaynews.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.bawie.todaynews.R;
+
+/**
+ * Created by Administrator on 2017/5/8.
+ * time:
+ * author:付智焱
+ */
+
+public class MentLeftFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view=inflater.inflate(R.layout.menuleft_fragment,container,false);
+        return  view;
+    }
+}
